782d0db80b934b450d2a1c21d53ce32df1943306
G384752023 <238475@st.takushoku-u.ac.jp>
2024-05-23 15:44:22 +0900

    a

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -18,7 +18,7 @@ let h2 = document.querySelector('h2#ex42');
 let p = document.createElement('p');
 p.textContent = '写真表と都市の緯度経度のページです';
 h2.insertAdjacentElement('afterend', p);
-p.style.color='sesame green'; 
+p.style.textEmphasis='sesame green'; 
 
 // 練習4-3 写真表作成プログラム
 
