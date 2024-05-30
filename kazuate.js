71b478ab97a9d9ec7701bde56cb820c721639ec5
G384752023 <238475@st.takushoku-u.ac.jp>
2024-05-30 16:55:25 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -17,8 +17,23 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
+  kaisu = kaisu + 1;
+  console.log(kaisu +'回目の予想'+ yoso);
+if(kaisu > 3){
+    console.log('答えは、'+ kotae +'でした。　すでにゲームは終わっています。');
+}else if(kaisu === 3){
+    console.log('まちがい。残念でした答えは'+ kotae +'です。');
+    if(kaisu <= 2 && yoso > kotae){
+        console.log('まちがい。答えはもっと小さいですよ。');
+    }else{
+        console.log('まちがい。答えはもっと大きいですよ。');
+    }
+    if(yoso === kotae){
+        console.log('正解です。おめでとう。');
+    }
+}
+}
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-}
