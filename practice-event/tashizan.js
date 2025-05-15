b4cbfa9e368948c00e2d5cd81848023be1c4ae11
G384752023 <238475@st.takushoku-u.ac.jp>
2025-05-15 17:25:40 +0900

    a

--- practice-event/tashizan.js
+++ practice-event/tashizan.js
@@ -8,14 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
       const leftValue = document.getElementById("left").value;
       const rightValue = document.getElementById("right").value;
   
-¥
+
       const leftNumber = parseInt(leftValue, 10);
       const rightNumber = parseInt(rightValue, 10);
   
 
       const answerSpan = document.getElementById("answer");
   
-¥
+
       if (isNaN(leftNumber) || isNaN(rightNumber)) {
         answerSpan.textContent = "エラー";
       } else {
