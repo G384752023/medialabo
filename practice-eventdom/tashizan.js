49df44cda266ce60804aee674f2de3a585b03cd2
G384752023 <238475@st.takushoku-u.ac.jp>
2025-06-04 22:22:52 +0900

    a

--- practice-eventdom/tashizan.js
+++ practice-eventdom/tashizan.js
@@ -4,7 +4,13 @@ window.addEventListener('DOMContentLoaded', function () {
     button.addEventListener('click', function () {
         const left = parseInt(document.getElementById('left').value, 10);
         const right = parseInt(document.getElementById('right').value, 10);
-        const answer = left + right;
-        document.getElementById('answer').textContent = answer;
+
+        // 計算できてる？
+        if (!isNaN(left) && !isNaN(right)) {
+            const answer = left + right;
+            document.getElementById('answer').textContent = answer;
+        } else {
+            document.getElementById('answer').textContent = '数値を入力してください';
+        }
     });
 });
