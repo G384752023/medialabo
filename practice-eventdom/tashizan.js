895a01e79cdbe1d521348d907ad56a712949324f
G384752023 <238475@st.takushoku-u.ac.jp>
2025-06-04 22:30:38 +0900

    a

--- practice-eventdom/tashizan.js
+++ practice-eventdom/tashizan.js
@@ -1,14 +1,14 @@
 function Add() {
-  let i = document.querySelector('input[name="left"]');
-  let j = document.querySelector('input[name="right"]');
+  let a = document.querySelector('input[name="left"]');
+  let b = document.querySelector('input[name="right"]');
   let left = i.value;
   let right = j.value;
   
-  let n = Number(left);
-  let o = Number(right);
-  let sum = n + o;
+  let c = Number(left);
+  let d = Number(right);
+  let sum = c + d;
   let ans = document.querySelector('span[id="answer"]');
   ans.textContent = sum;
 }
-let b = document.querySelector('button#calc');
-b.addEventListener('click', Add);
+let e = document.querySelector('button#calc');
+e.addEventListener('click', Add);
