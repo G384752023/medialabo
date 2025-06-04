1b98a0c0e41de48ce15baa9eab394ac5acdb57f8
G384752023 <238475@st.takushoku-u.ac.jp>
2025-06-04 22:20:54 +0900

    a

--- practice-eventdom/tashizan.js
+++ practice-eventdom/tashizan.js
@@ -4,6 +4,7 @@ window.addEventListener('DOMContentLoaded', function () {
     button.addEventListener('click', function () {
         const left = parseInt(document.getElementById('left').value, 10);
         const right = parseInt(document.getElementById('right').value, 10);
-
+        const answer = left + right;
+        document.getElementById('answer').textContent = answer;
     });
 });
